2de09700988efac4097d69b7a27f6d3d03170d96
Vered Rekanati <veredrec@gmail.com>
2017-07-19 12:27:37 +0300

    add wikipedia api call and add results to DOM Relates #6

--- public/script.js
+++ public/script.js
@@ -1,20 +1,30 @@
 var urlYouTube = "";
-// var urlWiki = "https://en.wikipedia.org/w/api.php?action=opensearch&search=israel&limit=1&format=json" + userSearch + "&format=json&callback=?";
-var youTube = document.querySelector("#description");
-var Wiki = document.querySelector("#");
 
+var wikiTitle = document.querySelector("#wikiTitle");
+var submit = document.querySelector("#submit");
+var form = document.getElementById("search-something");
 var wikiCall = new XMLHttpRequest();
-  wikiCall.onreadystatechange = function() {
-      if (wikiCall.readyState == 4 && wikiCall.status == 200) {
-        var wikiObj = JSON.parse(wikiCall.responseText);
+var youTube = document.querySelector("#description");
 
-        // var
-        // linkSpace.href = userLink;
-        // nameSapce.innerText = name;
-      };
-  };
-  wikiCall.open("GET", urlWiki, true);
-  wikiCall.send();
+wikiCall.onreadystatechange = function() {
+    if (wikiCall.readyState == 4 && wikiCall.status == 200) {
+      var wikiObj = JSON.parse(wikiCall.responseText);
+      var title = wikiObj.query.pages[Object.keys(wikiObj.query.pages)[0]].title;
+      wikiTitle.innerText = title;
+      var abstract = wikiObj.query.pages[Object.keys(wikiObj.query.pages)[0]].extract;
+      wikiAbstract.innerText = abstract;
+    };
+};
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var userSearch = document.querySelector("#description").value;
+    form.reset();
+    var urlWiki = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + userSearch +"";
+    wikiCall.open("GET", urlWiki, true);
+    wikiCall.send();
+});
+
 
 
 
